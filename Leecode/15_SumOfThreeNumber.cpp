1b33755a29b5e1bf7dadf0da52a6e5dcfb505d65
Hyz <13592815435@163.com>
2025-05-13 13:29:51 +0800

    0513_11&15

---
+++ Leecode/15_SumOfThreeNumber.cpp
@@ -0,0 +1,72 @@
+// 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
+// 注意：答案中不可以包含重复的三元组。
+
+// 示例 1：
+// 输入：nums = [-1,0,1,2,-1,-4]
+// 输出：[[-1,-1,2],[-1,0,1]]
+// 解释：
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+// 不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+// 注意，输出的顺序和三元组的顺序并不重要。
+
+// 示例 2：
+// 输入：nums = [0,1,1]
+// 输出：[]
+// 解释：唯一可能的三元组和不为 0 。
+
+// 示例 3：
+// 输入：nums = [0,0,0]
+// 输出：[[0,0,0]]
+// 解释：唯一可能的三元组和为 0 。
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+    if(nums.size() < 3) return {};
+    std::sort(nums.begin(), nums.end());
+    std::vector<std::vector<int>> result;
+    for (int i = 0; i < nums.size() - 2; i++)
+    {
+        if(nums[i] > 0) break;
+        if(i > 0 && nums[i] == nums[i - 1]) continue;
+        int left = i + 1;
+        int right = nums.size() - 1;
+        int target = 0 - nums[i];
+        
+        while(left < right)
+        {
+            if(nums[left] + nums[right] == target)
+            {
+                std::vector<int> res = {nums[i], nums[left], nums[right]};
+                result.push_back(res);
+                while(left < right && nums[left] == nums[left + 1]) left++;
+                while(left < right && nums[right] == nums[right - 1]) right--;
+                left++;
+                right--;
+            }
+            else if(nums[left] + nums[right] < target)
+                left++;
+            else
+                right--;
+        }
+    }
+    return result;
+}
+
+// int main()
+// {
+//     std::vector<int> nums = {-1,0,1,2,-1,-4};
+//     auto res = threeSum(nums);
+//     for(auto row = res.begin(); row != res.end(); row++)
+//     {
+//         for(auto it = row->begin(); it != row->end(); it++)
+//             std::cout << *it << " ";
+//         std::cout << "\n";
+//     }
+//     return 0;
+// }
